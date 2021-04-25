7fc728817870ba463156e455342ee7b0014bfc8f
Michael Descamps <Michael.Descamps@swisscom.com>
2021-04-25 22:28:56 +0200

    create test project

---
+++ MarsPanel.Tests/Services/DataServiceTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MarsPanel.Tests.Services
+{
+    public class DataServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
